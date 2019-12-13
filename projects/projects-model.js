64db91746b104daa55bac4f4514c1a0e9438bce2
Hugo Oliveira <oliveira.hugo.c@gmail.com>
2019-12-13 12:06:57 -0600

    task end points. Need to update combination table

--- projects/projects-model.js
+++ projects/projects-model.js
@@ -2,7 +2,12 @@ const db = require("../data/db-config.js");
 
 module.exports = {
   find,
-  findById
+  findById,
+  add,
+  findAllTasks,
+  findTaskById,
+  findTasksByProjectId,
+  addTask
 };
 
 function find() {
@@ -13,4 +18,58 @@ function findById(id) {
   return db("projects")
     .where({ id })
     .first();
+}
+
+function add(project) {
+  return db("projects")
+    .insert(project, "id")
+    .then(([id]) => {
+      return findById(id);
+    });
+}
+
+function findAllTasks() {
+  return db("tasks");
+}
+
+function findTaskById(id) {
+  return db("tasks")
+    .where({ id })
+    .first();
+}
+
+function findTasksByProjectId(id) {
+  console.log(id);
+  /*SELECT t.id, t.description, t.notes, t.completed, p.id as project_id, p.name as project_name, p.description as project_description
+FROM projects_tasks as pt
+JOIN tasks as t ON pt.task_id = t.id
+JOIN projects as p ON pt.project_id = p.id
+WHERE pt.project_id = 2*/
+  return db("projects_tasks as pt")
+    .select(
+      "t.id",
+      "t.description",
+      "t.notes",
+      "t.completed",
+      "p.id as project_id",
+      "p.name as project_name",
+      "p.description as project_description"
+    )
+    .join("tasks as t", "pt.task_id", "t.id")
+    .join("projects as p", "pt.project_id", "p.id")
+    .where("pt.project_id", id);
+}
+
+function addTask(task, projectId) {
+  return db("tasks")
+    .insert(task, "id")
+    .then(([id]) => {
+      const project_task = { project_id: projectId, task_id: id };
+      console.log(project_task);
+      console.log(db("projects_tasks").insert(project_task));
+      return id;
+    })
+    .then(id => {
+      return findTaskById(id);
+    });
 }
--- projects/projects-router.js
+++ projects/projects-router.js
@@ -4,6 +4,7 @@ const Projects = require("./projects-model.js");
 
 const router = express.Router();
 
+// ###PROJECTS###
 router.get("/projects", (req, res) => {
   Projects.find()
     .then(projects => {
@@ -32,6 +33,97 @@ router.get("/projects/:id", (req, res) => {
       console.log(err);
       res.status(500).json({ errorMessage: "Failed to get project" });
     });
+});
+
+router.post("/projects", (req, res) => {
+  const newProject = req.body;
+
+  if (!newProject.name) {
+    res.status(400).json({
+      errorMessage: "Please provide a name for the project."
+    });
+  } else {
+    Projects.add(newProject)
+      .then(project => {
+        res.status(201).json(project);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json({
+          errorMessage: "Failed to add project"
+        });
+      });
+  }
+});
+
+// ###TASKS###
+router.get("/tasks", (req, res) => {
+  Projects.findAllTasks()
+    .then(tasks => {
+      res.status(200).json(tasks);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ errorMessage: "Failed to get tasks" });
+    });
+});
+
+router.get("/tasks/:id", (req, res) => {
+  const { id } = req.params;
+
+  Projects.findTaskById(id)
+    .then(task => {
+      if (task) {
+        res.status(200).json(task);
+      } else {
+        res.status(404).json({ message: "Could not find task with given id." });
+      }
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ errorMessage: "Failed to get task" });
+    });
+});
+
+router.get("/projects/:id/tasks", (req, res) => {
+  const { id } = req.params;
+
+  Projects.findTasksByProjectId(id)
+    .then(tasks => {
+      if (tasks.length) {
+        res.status(200).json(tasks);
+      } else {
+        res
+          .status(404)
+          .json({ errorMessage: "Could not find project with given id." });
+      }
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ errorMessage: "Failed to get tasks" });
+    });
+});
+
+router.post("/projects/:id/tasks", (req, res) => {
+  const { id } = req.params;
+  const newTask = req.body;
+
+  if (!newTask.description) {
+    res.status(400).json({
+      errorMessage: "Please provide a description for the task."
+    });
+  } else {
+    Projects.addTask(newTask, id)
+      .then(task => {
+        res.status(201).json(task);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json({
+          errorMessage: "Failed to add task"
+        });
+      });
+  }
 });
 
 module.exports = router;
